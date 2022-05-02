386ee4d7da2264fb99cc39cde2adeb0838708a0e
moolnam <moolnam>
2022-05-02 22:11:40 +0900

    1 비트코인 시세 불러오기 각 나라별로 API free 버전 요청 100회 넘어서 지금 현재 값이 안나오는 것 같다.

--- Hit Coin App/Model/CoinManager.swift
+++ Hit Coin App/Model/CoinManager.swift
@@ -8,13 +8,23 @@
 import Foundation
 
 
+protocol CoinManagerDelegate {
+    
+    func didUpdatePrice(price: String, currency: String)
+    func didFailWithError(error: Error)
+}
+
 struct CoinManager {
+    
+    var delegate: CoinManagerDelegate?
+    
     let baseUrl = "https://rest.coinapi.io/v1/exchangerate/BTC"
     let myApiKey = "6C380E51-4038-4668-9E23-DC240C2C709F"
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 
     func getCoinPrice(for currency: String) {
         let urlString = "\(baseUrl)/\(currency)?apikey=\(myApiKey)"
+        print(urlString)
         
         if let url = URL(string: urlString) {
             // url 에다 urlString 담기
@@ -22,19 +32,40 @@ struct CoinManager {
             // session 안에다 URLSeesion 디폴트 값으로 담기
             let task = sesseion.dataTask(with: url) { (data, response, error) in
                 if error != nil {
-                    print(error)
-                    return
+                    delegate?.didFailWithError(error: error!)
                 }
                 if let safeData = data {
-                    
+                    // 비트코인 가격이 있다면
+                    if let bitCoinPrice = self.parseJSON(safeData) {
+                        // bitCoinProce 안에 parseJSON 함수를 불러오는데
+                        // 파라메터 안에 Data 타입이 있다. 그 Data 타입을 Double? 변환시켜서 내보낸다.
+                        let bitCoinPriceString = String(format: "%.2f", bitCoinPrice)
+                        delegate?.didUpdatePrice(price: bitCoinPriceString, currency: currency)
+                    }
                 }
             }
+            task.resume()
         }
             
             
     }
     
-    func prearJSON(_ Data: data) -> Double? {
+    func parseJSON(_ data: Data) -> Double? {
+        let decoder = JSONDecoder()
         
+        do {
+            let decodedData = try decoder.decode(CoinData.self, from: data)
+            // decodedData 안에 decoder.decode() 함수를 넣는다 그 함수 안에 CoinData 라는 데이터를 넣는다. decode() 함수는 Decodable 프로토콜 이라는 것 안에 있는 함수이다.
+            let lastPrice = decodedData.rate
+            // 마지막 가격은 CoinData 안에 있는 rate를 가져온다.
+            print(lastPrice)
+            return lastPrice
+            // 반드시 리턴을 시켜줘야 한다.
+        } catch {
+            delegate?.didFailWithError(error: error)
+            // 프로토콜로 만든 CoinManagerDelegate 함수로 에러를 잡는다.
+            return nil
+            // 반드시 리턴을 시켜줘야 한다.
+        }
     }
 }
--- Hit Coin App/View/ViewController.swift
+++ Hit Coin App/View/ViewController.swift
@@ -9,7 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
     var coinManager = CoinManager()
+    // CoinManager 안에 API 데이터를 받아 올 수 있게 하는 것이 들어 있다.
     
     @IBOutlet weak var bitLabel: UILabel!
     @IBOutlet weak var currencyLabel: UILabel!
@@ -19,30 +21,61 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        coinManager.delegate = self
+        // CoinManager를 쓸수 있게 한다
         bitPicker.delegate = self
+        // UIPickerViewDelegate
         bitPicker.dataSource = self
+        // UIPickerViewDataSource
     }
     
-    
-
-
 }
+
+//MARK: - CoinManagerDelegate
+
+extension ViewController: CoinManagerDelegate {
+    // 프로토콜인 CoinManagerDelegate
+    func didUpdatePrice(price: String, currency: String) {
+        // 그 안에 함수가 담겨 있는데 CoinManagerDelegate를 CoinManager 안에다 넣고 편리하게 가져다 쓴다.
+        DispatchQueue.main.async {
+            // 비동기 혹은 동기적인 무엇인가 인것 같다.
+            self.bitLabel.text = price
+            // 데이터가 있다면 함수 안에 담겨서 price에 스트링 타입으로 담긴다.
+            self.currencyLabel.text = currency
+            // 데이터가 있다면 현재 선택한 통화종류가 담겨서 온다.
+        }
+    }
+    
+    func didFailWithError(error: Error) {
+        print(error)
+    }
+}
+
+
+//MARK: - UIPickerViewDataSource, UIPickerViewDelegate
 
 extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
+        // 줄이 1줄이다 2줄로 바꾸면 충돌하는 것같다.
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        
+        return coinManager.currencyArray.count
+        // 현재 CoinManager 안에 있는 다양한 통화 종류의 갯수만큼 불러온다.
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        coinManager.currentArrary[row]
+        return coinManager.currencyArray[row]
+        // CoinManager 안에 있는 딕셔너리 안에 있는 스트링 타입을 row로 가져온다.
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        coinManager.currentArrary.last
+        let selectedCurrency = coinManager.currencyArray[row]
+        // row 중 선택 된 것을 selectedCurrency 안에 담는다.
+        coinManager.getCoinPrice(for: selectedCurrency)
+        // 여기서 실행 된다.
+        // CoinManager 안에 있는 함수 getCoinPrice() for 안에 selectedCurrency를 담는다. 이것은 스트링 타입으로 통화 종류이다. 
     }
     
 }
